93975f63c7eacf56d8d64ef3260e0a2eca3209cf
Lilit Movsisyan <lilit.momo@gmail.com>
2018-10-07 14:54:11 +0100

    Create Entry model, add to view; not yet migrated

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length = 200)
+    #author = 
+    date_published = models.DateTimeField
+    text = models.TextField
+    #attachments = 
+    #tags = 
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from .models import Entry
 
 # Create your views here.
 def home(request):
     #return HttpResponse("So what's the issue?")
+    entry = Entry
 
-    return render(request, 'blog/home.html', {})
+    return render(request, 'blog/home.html', {'entry': Entry})
