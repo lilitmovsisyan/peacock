6d6649c3b44e7ccf5a06fc1b803bb2f162bbc956
Lilit Movsisyan <lilit.momo@gmail.com>
2018-10-07 20:16:12 +0100

    Render data objects in home.html. BUT no TextField

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Entry(models.Model):
     title = models.CharField(max_length = 200)
     #author = 
-    date_published = models.DateTimeField
-    text = models.TextField
+    date_published = models.DateTimeField(default=timezone.now) #need to import django.utils.timezone and use timezone.now WITHOUT brackets after now().
+    #entry_text = models.TextField()
     #attachments = 
     #tags = 
+
+    def __repr__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,17 @@ from django.shortcuts import render
 from .models import Entry
 
 # Create your views here.
+
+"""
 def home(request):
     #return HttpResponse("So what's the issue?")
     entry = Entry
 
     return render(request, 'blog/home.html', {'entry': entry})
+"""
+
+def home(request):
+    # retrieve all Entry objects from database
+    entries_list = [e for e in Entry.objects.all()] 
+    # pass the first 10 entries for rendering in the home template
+    return render(request, 'blog/home.html', {'entries_list': entries_list[:10]})
