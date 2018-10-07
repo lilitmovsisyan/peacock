2d4b82f4f6f9197212240be92bafeb15aebf958d
Lilit Movsisyan <lilit.momo@gmail.com>
2018-10-07 14:31:17 +0100

    Create working template 'blog/home.html'

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 def home(request):
-    return HttpResponse('Testing')
+    #return HttpResponse("So what's the issue?")
+
+    return render(request, 'blog/home.html', {})
