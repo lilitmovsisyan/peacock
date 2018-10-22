0dd7c8afe5012a6522fa709606e048aefea82cc9
Lilit Movsisyan <lilit.momo@gmail.com>
2018-10-22 12:11:02 +0100

    Add author field to Entry model

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class Entry(models.Model):
     title = models.CharField(max_length = 200)
-    #author = 
+    author = models.CharField(max_length = 200)
     date_published = models.DateTimeField(default=timezone.now) #need to import django.utils.timezone and use timezone.now WITHOUT brackets after now().
     text = models.TextField(default="")
     #attachments = 
