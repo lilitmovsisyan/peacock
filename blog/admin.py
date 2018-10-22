fe070b58a9cf9fdc07dbd670c979ed9d3f1e40bf
Lilit Movsisyan <lilit.momo@gmail.com>
2018-10-22 16:08:03 +0100

    Create Image model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Entry, Tag
+from .models import Entry, Tag, Image
 
 # Register your models here.
 admin.site.register(Entry)
 admin.site.register(Tag)
+admin.site.register(Image)
--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,12 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+class Image(models.Model):
+    image = models.ImageField(upload_to='', null=True)
+    name = models.CharField(max_length = 100, default="")
+    entry = models.ForeignKey('Entry', on_delete=models.CASCADE)
+
+
 class Entry(models.Model):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length = 200)
@@ -25,4 +31,16 @@ class Entry(models.Model):
 
 
 
+
 #For help, look up related objects, model fields, 
+
+"""Consider editing path images are saved to using this django example:
+For example:
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class MyModel(models.Model):
+    upload = models.FileField(upload_to=user_directory_path)
+"""
