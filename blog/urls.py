9c7b0fc5b107fc7c15db03e17d4be3a88b6e488c
Lilit Movsisyan <lilit.momo@gmail.com>
2019-01-29 14:57:27 +0000

    correct typo in urls.py: path("bio/"...)

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, {}, name='home'),
     path('index/', views.index, name='index'),
-        path('bio', views.bio, name='bio'),
+    path('bio/', views.bio, name='bio'),
     path('project<int:project_id>/<str:project_title>/', views.project, name='project')
 ]
